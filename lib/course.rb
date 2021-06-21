2300959a02ea424ad9cf3b9fde88596ba0887f6a
Yisrael Gurkow <yisrelg3@gmail.com>
2021-06-21 17:41:13 -0400

    commit tree bb9443c6509fe285256d3a80b5d6118262167630

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,17 @@
+class Course
+    @@all = []
 
+    def self.all
+        @@all
+    end
+
+    def self.reset_all
+        self.all.clear
+    end
+
+    attr_accessor :title, :schedule, :description
+    def initialize
+        self.class.all << self
+    end
+end
 
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,32 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
   
+  def get_page
+    doc = Nokogiri::HTML(URI.open("http://learn-co-curriculum.github.io/site-for-scraping/courses")) 
+
+    # 
+
+    # binding.pry
+  end
+
+  def get_courses
+   self.get_page.search('.post')
+  end
+
+  def make_courses
+    self.get_courses.map do |post|
+        course = Course.new
+        course.title = post.search('h2').text
+        course.schedule = post.search('.date').text
+        course.description = post.search('p').text
+      end
+  end
+
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -18,5 +40,5 @@ class Scraper
   
 end
 
-
+Scraper.new.print_courses
 
